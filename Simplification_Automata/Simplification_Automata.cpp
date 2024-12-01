4d75fa791dac55abfaa56769ce0cd14e55097170
YavuzEG <yavuzemregunduz@gmail.com>
2024-12-01 11:45:55 +0300

    Proje dosyası ekle.

---
+++ Simplification_Automata/Simplification_Automata.cpp
@@ -0,0 +1,10 @@
+﻿// Simplification_Automata.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
